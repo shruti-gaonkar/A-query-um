f3a1974e9cd8f0af1609a8e0f9b6c9f1565ddb7b
origami unicorn <rvwright@gmail.com>
2020-03-10 22:11:26 -0700

    Updated findBy with params.

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.route('/list')
     .get(controller.findAll);
 
-router.route('/search?q=:query')
+router.route('/search/:query')
     .get(controller.findBy);
 
 router.route('/create')
