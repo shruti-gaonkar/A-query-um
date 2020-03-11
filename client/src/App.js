8f9290dfcd1ecd2323e4748a90d3419ffdaa5109
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-03-10 20:31:34 -0700

    Removed react-hook-form from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,8 @@ import Nav from './components/Nav';
 import ParallaxContainer from './components/ParallaxContainer';
 import ScrapeContainer from './components/ScrapeContainer';
 import PageFooter from './components/PageFooter';
-import { useForm } from 'react-hook-form'
 
 function App() {
-  const { register, handleSubmit, watch, errors } = useForm()
-  const onSubmit = data => { console.log(data) }
-
-  console.log(watch('email_address')) // watch input value by passing the name of it
-
   return (
     <>
       <Nav />
