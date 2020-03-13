21df08bd699bb8cf218c877270bc8999458d6f31
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-03-12 21:41:24 -0700

    Added useLayoutEffect for searchcontainer

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import { Container, Card, Icon, CardTitle, Row, Col } from 'react-materialize';
 import API from "../utils/API";
 import FishList from "../components/FishList";
@@ -7,17 +7,22 @@ function SearchContainer(props) {
     const [hasError, setErrors] = useState(false);
     const [results, setResults] = useState({});
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         //alert(props.query);
         loadResults();
     });
 
     const loadResults = () => {
+        API.listBy(props.query)
+            .then(res => { console.log(res) })
+    }
+
+    /*const loadResults = () => {
         API.list()
             .then(res => { res.json(); console.log(res) })
             .then(res => setResults(res))
             .catch(err => setErrors(err));
-    }
+    }*/
 
     return (
         <Container>
