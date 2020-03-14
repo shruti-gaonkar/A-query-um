de9c5d068674bba1ed06806fa35639081da1d9ba
origami unicorn <rvwright@gmail.com>
2020-03-14 14:12:50 -0700

    Working on changing display of information on FishDetail.

--- client/src/components/FishDetail.js
+++ client/src/components/FishDetail.js
@@ -1,21 +1,58 @@
 import React from "react";
-import { Card, CardTitle } from 'react-materialize';
+import { Row, Col, Card, CardTitle, Carousel } from 'react-materialize';
 
 function FishDetail(props) {
     const { fish } = props;
-    console.log("these are the fish details" + props)
+
+    let allImgs = fish.images;
+
+    const makeCarousel = function (images) {
+        let arr = [];
+        for (let i = 0; i < images.length; i++) {
+            arr.push(images[i])
+        }
+    }
+
+    console.log("these are all the images", allImgs);
+
+    console.log("these are the fish details", { fish })
     return (
-        <Card id={fish._id} key={fish._id} className="blue-grey darken-2 white-text"
-            actions={[
-                <div>{fish.aliases[0]}</div>
-            ]}
-            header={<CardTitle image={fish.images[0].img === undefined
-                ? ""
-                : `${fish.images[0].img} `} className="cardImg" />}
-            horizontal
-        >
-            {fish.description}
-        </Card >
+        <>
+            <Card id={fish._id} key={fish._id} className="blue-grey darken-2 white-text"
+                actions={[
+                    <div>{fish.aliases[0]}</div>
+                ]}
+                header={<CardTitle image={fish.images[0].img === undefined
+                    ? ""
+                    : `${fish.images[0].img} `} className="cardImg" />}
+                horizontal
+            >
+                {fish.description}
+            </Card >
+
+            <Carousel
+                images={[
+                    fish.images[0].img
+                ]}
+                options={{
+                    dist: -100,
+                    duration: 200,
+                    fullWidth: false,
+                    indicators: true,
+                    noWrap: false,
+                    numVisible: 5,
+                    onCycleTo: null,
+                    padding: 0,
+                    shift: 0
+                }}
+            />
+
+            <Row>
+                <Col>
+
+                </Col>
+            </Row>
+        </>
     );
 }
 
