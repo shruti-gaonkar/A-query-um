525f5f9204c5d851702838a599c7e9ba8e65e172
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-03-14 03:41:15 -0700

    Added navigate function of hookrouter onclick of search button

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useRoutes, navigate } from 'hookrouter';
 import { Row, Col, Button, Icon } from 'react-materialize';
 import Input from "./Input";
 import API from "../utils/API";
@@ -7,20 +8,14 @@ import API from "../utils/API";
 function Search() {
     const [query, setQuery] = useState('');
     const { register, handleSubmit, watch, errors } = useForm()
-    /*const onSubmit = data => {
-        API.login();
-        console.log(data)
-    }*/
-
-    const handleSearch = () => {
-        //setQuery()
-        //hookrouter.setPath(`'/search/${query}'`);
-        //context.router.push(`'/search/${query}'`);
+    const onSubmit = data => {
+        const href = `/search/${data.search}`;
+        navigate(href, true);
     }
 
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <Row>
                     <Col m={6} s={6}>
                         <Input className="orange" label="Search" name="search" inputRef={register({
@@ -30,7 +25,7 @@ function Search() {
 
                     </Col>
                     <Col m={6} s={6}>
-                        <Button className="red" type="submit" onClick={handleSearch}>
+                        <Button className="red" type="submit">
                             Search
                     <Icon right>
                                 search
