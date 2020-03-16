7afffcad63a69b1abbedd9c3ad55e17917c1d9d8
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-03-15 22:14:01 -0700

    Added route and its controller functionality to scrape news data

---
+++ server/controllers/api/news.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+module.exports = {
+    scrapeData: (req, res) => {
+        axios.get("https://www.theaquariumguide.com/aquarium-fish-care-guides").then(function (response) {
+            var $ = cheerio.load(response.data);
+            var results = [];
+            $("article.elementor-post").each(function (i, element) {
+                var title = $(element).find(".elementor-post__title").text().trim();
+                var link = $(element).find("a").attr("href");
+                var img = $(element).find("img").attr("src");
+                results.push({
+                    title: title,
+                    link: link,
+                    image: img
+                });
+            });
+            res.send(results);
+        });
+    }
+};
