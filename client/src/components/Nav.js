dfab2dbe9080339b5d7a49cb1c2dc30d557e1934
origami unicorn <rvwright@gmail.com>
2020-03-19 19:26:46 -0700

    Updated Nav.js for build compat.

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Navbar, Icon, NavItem } from 'react-materialize';
 import LoginModal from './LoginModal';
