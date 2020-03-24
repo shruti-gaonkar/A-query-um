62829c8a0e020304963e701e02073799d2ce274d
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-03-24 00:20:01 -0700

    Added an adminType column for user model

--- server/models/User/userSchema.js
+++ server/models/User/userSchema.js
@@ -20,6 +20,10 @@ const userSchema = new Schema({
         required: true
     },
     username: String,
+    adminType: {
+        type: Boolean,
+        default: false
+    },
     fishes: [
         {
             // Store ObjectIds in the array
