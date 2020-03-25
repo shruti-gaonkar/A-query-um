084cdc62665f013048151ffc5e4553d1b8c26ec6
origami unicorn <rvwright@gmail.com>
2020-03-25 15:02:03 -0700

    Changing how search box was centered on page.

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { useRoutes, navigate } from 'hookrouter';
+import { navigate } from 'hookrouter';
 import { Row, Col, Button, Icon } from 'react-materialize';
 import Input from "./Input";
 
 const rowStyle = {
-    marginBottom: 0
+    marginBottom: "10px"
 };
 const searchStyle = {
     background: "rgba(255, 255, 225, 0.7)",
@@ -23,24 +23,27 @@ function Search() {
 
     return (
         <>
-            <Row style={rowStyle}>
-                <form className="valign-wrapper" onSubmit={handleSubmit(onSubmit)}>
-                    <Col className="center-align s10 m5">
-                        <Input className="darkgrey-text" name="search" style={searchStyle} inputRef={
-                            register({ required: true })}
-                        />
-
-                    </Col>
-                    <Col className="center-align">
-                        <Button className="cyan darken-3 btn-floating" style={{ borderRadius: '25px' }} type="submit">
-                            <Icon>
-                                search
+            <Row>
+                <Col className="s12 m6 offset-m3">
+                    <Row style={rowStyle}>
+                        <form className="valign-wrapper" onSubmit={handleSubmit(onSubmit)}>
+                            <Col s={12}>
+                                <Input className="darkgrey-text" name="search" style={searchStyle} inputRef={
+                                    register({ required: true })}
+                                />
+                            </Col>
+                            <Col className="center-align">
+                                <Button className="cyan darken-3 btn-floating" style={{ borderRadius: '25px' }} type="submit">
+                                    <Icon>
+                                        search
                             </Icon>
-                        </Button>
-                    </Col>
-                    <Col className="offset-m3 hide-on-small-only">
-                    </Col>
-                </form>
+                                </Button>
+                            </Col>
+                            {/* <Col className="offset-m3 hide-on-small-only">
+                    </Col> */}
+                        </form>
+                    </Row>
+                </Col>
             </Row>
         </>
     );
