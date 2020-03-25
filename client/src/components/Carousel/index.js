bf32d22ab902b5621203ed458f7dfc882e386290
cgenel <genel661@gmail.com>
2020-03-24 18:20:32 -0700

    added scroll to top button in footer

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -1,22 +1,22 @@
-// import React from "react";
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
-// import { Carousel } from 'react-responsive-carousel';
-// import '../Carousel/styles.css';
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import '../Carousel/styles.css';
 
-// function CarouselContainer(props) {
-//     const { images } = props;
+function CarouselContainer(props) {
+    const { images } = props;
 
-//     const makeCarousel = images.map(({ img, alt }) =>
-//         <div>
-//             <img src={img} alt={alt} />
-//             <p className="legend">{alt}</p>
-//         </div>)
+    const makeCarousel = images.map(({ img, alt }) =>
+        <div>
+            <img src={img} alt={alt} />
+            <p className="legend">{alt}</p>
+        </div>)
 
-//     return (
-//         <Carousel infiniteLoop emulateTouch showThumbs={false} width="auto">
-//             {makeCarousel}
-//         </Carousel>
-//     );
-// }
+    return (
+        <Carousel infiniteLoop emulateTouch showThumbs={false} width="auto">
+            {makeCarousel}
+        </Carousel>
+    );
+}
 
-// export default CarouselContainer;
+export default CarouselContainer;
